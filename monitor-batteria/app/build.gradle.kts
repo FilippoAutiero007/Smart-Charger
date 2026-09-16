0fcf3f7de93aed0e7c33f983ffbf9261c0d81a8d
Filippo <filippoautiero07@gmail.com>
2026-09-16 20:49:25 +0200

    feat: v2.3 loginUrl diretto + soglie bloccate se non loggato + Gmail 587

--- monitor-batteria/app/build.gradle.kts
+++ monitor-batteria/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "com.aistudio.batterymonitor.kzhpqr"
     minSdk = 24
     targetSdk = 36
-    versionCode = 7
-    versionName = "2.2"
+    versionCode = 8
+    versionName = "2.3"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
